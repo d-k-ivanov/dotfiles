fb037eab0826fbef39c2ef0ed0145b57a00e9bb5
Dmitry Ivanov <d.k.ivanov@live.com>
2021-12-16 18:05:44 +0100

    add now functions

--- powershell/Autoload/Unix.ps1
+++ powershell/Autoload/Unix.ps1
@@ -53,3 +53,6 @@ function time
 ${function:bash-wsl}    = { conemu-cyg-64.exe --wsl -cur_console:h0 }
 
 Set-Alias wc Measure-Object
+
+${function:now}     = { Write-Output $(Get-Date -Format "yyyy-dd-MM-HH-mm") }
+${function:now_gnu} = { Write-Output $(date +%Y-%m-%d-%H-%M) }
