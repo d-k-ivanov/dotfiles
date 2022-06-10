0b0e0b75bbe10141a96cae630863dbf13ace2c7a
Dmitry Ivanov <d.k.ivanov@live.com>
2022-06-10 18:16:45 +0200

    dotfiles:

--- bash/autoload/kubernetes.sh
+++ bash/autoload/kubernetes.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 alias     kdev='kubectl --context dev'
+alias  kdevops='kubectl --context devopseu'
 alias   kstage='kubectl --context stage'
 alias    kprod='kubectl --context prod'
 alias   kdevcn='kubectl --context devcn'
@@ -9,6 +10,7 @@ alias  kprodcn='kubectl --context prodcn'
 alias      kws='kubectl --context docker-desktop'
 
 alias     hdev='helm --kube-context dev'
+alias  hdevops='helm --kube-context devopseu'
 alias   hstage='helm --kube-context stage'
 alias    hprod='helm --kube-context prod'
 alias   hdevcn='helm --kube-context devcn'
@@ -17,6 +19,7 @@ alias  hprodcn='helm --kube-context prodcn'
 alias      hws='helm --kube-context docker-desktop'
 
 alias     kdev_proxy='kdev      proxy --port=10001'
+alias  kdevops_proxy='kdevops   proxy --port=10001'
 alias   kstage_proxy='kstage    proxy --port=10001'
 alias    kprod_proxy='kprod     proxy --port=10001'
 alias   kdevcn_proxy='kdevcn    proxy --port=10001'
@@ -25,6 +28,7 @@ alias  kprodcn_proxy='kprodcn   proxy --port=10001'
 alias      kws_proxy='kdocker   proxy --port=10001'
 
 alias     kdev_port_fwd='kdev     port-forward --v=6 --address 0.0.0.0 service/'
+alias  kdevops_port_fwd='kdevops  port-forward --v=6 --address 0.0.0.0 service/'
 alias   kstage_port_fwd='kstage   port-forward --v=6 --address 0.0.0.0 service/'
 alias    kprod_port_fwd='kprod    port-forward --v=6 --address 0.0.0.0 service/'
 alias   kdevcn_port_fwd='kdevcn   port-forward --v=6 --address 0.0.0.0 service/'
@@ -33,6 +37,7 @@ alias  kprodcn_port_fwd='kprodcn  port-forward --v=6 --address 0.0.0.0 service/'
 alias      kws_port_fwd='kdocker  port-forward --v=6 --address 0.0.0.0 service/'
 
 alias     kdev_consul='kdev     port-forward service/consul 8300 8301 8302 8500 8600'
+alias  kdevops_consul='kdevops  port-forward service/consul 8300 8301 8302 8500 8600'
 alias   kstage_consul='kstage   port-forward service/consul 8300 8301 8302 8500 8600'
 alias    kprod_consul='kprod    port-forward service/consul 8300 8301 8302 8500 8600'
 alias   kdevcn_consul='kdevcn   port-forward service/consul 8300 8301 8302 8500 8600'
@@ -41,6 +46,7 @@ alias  kprodcn_consul='kprodcn  port-forward service/consul 8300 8301 8302 8500 
 alias      kws_consul='kdocker  port-forward service/consul 8300 8301 8302 8500 8600'
 
 alias     kdev_rabbit='kdev     port-forward service/rabbitmq-ha 4369 5672 15672 15692'
+alias  kdevops_rabbit='kdevops  port-forward service/rabbitmq-ha 4369 5672 15672 15692'
 alias   kstage_rabbit='kstage   port-forward service/rabbitmq-ha 4369 5672 15672 15692'
 alias    kprod_rabbit='kprod    port-forward service/rabbitmq-ha 4369 5672 15672 15692'
 alias   kdevcn_rabbit='kdevcn   port-forward service/rabbitmq-ha 4369 5672 15672 15692'
@@ -51,6 +57,11 @@ alias      kws_rabbit='kdocker  port-forward service/rabbitmq-ha 4369 5672 15672
 function kdev_exec()
 {
     kdev exec -it $@ -- sh -c '(bash || ash || sh)'
+}
+
+function kdevops_exec()
+{
+    kdevops exec -it $@ -- sh -c '(bash || ash || sh)'
 }
 
 function kstage_exec()
@@ -84,6 +95,7 @@ function kws_exec()
 }
 
 # alias     kdev_admin="kdev     -n kube-system describe secret $(kdev     -n kube-system get secret | grep admin-user-token | awk '{print $1}')"
+# alias  kdevops_admin="kdevops  -n kube-system describe secret $(kdevops  -n kube-system get secret | grep admin-user-token | awk '{print $1}')"
 # alias   kstage_admin="kstage   -n kube-system describe secret $(kstage   -n kube-system get secret | grep admin-user-token | awk '{print $1}')"
 # alias    kprod_admin="kprod    -n kube-system describe secret $(kprod    -n kube-system get secret | grep admin-user-token | awk '{print $1}')"
 # alias   kdevcn_admin="kdevcn   -n kube-system describe secret $(kdevcn   -n kube-system get secret | grep admin-user-token | awk '{print $1}')"
@@ -94,4 +106,3 @@ function kimages
 {
     echo "WIP"
 }
-
