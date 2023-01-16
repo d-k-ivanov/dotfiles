153554c03e86ee30c90caa1addc744ca44c17b57
Dmitry Ivanov <d.k.ivanov@live.com>
2023-01-16 22:27:54 +0100

    add python aliaes to powershell for unix

---
+++ powershell/AutoloadUnix/Python.ps1
@@ -0,0 +1,67 @@
+<#
+.SYNOPSIS
+Python scripts.
+
+.DESCRIPTION
+Python scripts.
+#>
+
+# Check invocation
+if ($MyInvocation.InvocationName -ne '.')
+{
+    Write-Host `
+        "Error: Bad invocation. $($MyInvocation.MyCommand) supposed to be sourced. Exiting..." `
+        -ForegroundColor Red
+    Exit
+}
+
+if (Get-Command python -ErrorAction SilentlyContinue | Test-Path)
+{
+    ${function:vc}      = { ($python = Get-Command python | Select-Object -ExpandProperty Definition); python -m virtualenv -p $python venv }
+    ${function:va}      = { .\venv\Scripts\activate }
+    ${function:vd}      = { deactivate }
+    ${function:vr}      = { rmrf venv }
+    ${function:vpi}     = { python -m pip install  }
+    ${function:vins}    = { python -m pip install -r .\requirements.txt }
+    ${function:vgen}    = { python -m pip freeze > .\requirements.txt }
+
+    # Basic environment
+    ${function:pip-update}      = { python -m pip install --upgrade pip }
+    ${function:venv-install}    = { python -m pip install virtualenv }
+    ${function:ipython-install} = { python -m pip install ipython }
+
+    function py_init_environmnet
+    {
+        python -m pip install --upgrade pip
+        python -m pip install --upgrade flake8
+        python -m pip install --upgrade ipython
+        python -m pip install --upgrade pytest
+        python -m pip install --upgrade virtualenv
+        python -m pip install --upgrade cfn-lint
+    }
+
+    function pyclean
+    {
+        [CmdletBinding()]
+        param
+        (
+            [switch] $SkipInit
+        )
+        [string] $SessionID = [System.Guid]::NewGuid()
+        $TempFreezeFile  = (Join-Path "${Env:Temp}" "${SessionID}")
+        python -m pip freeze > "${TempFreezeFile}"
+        python -m pip uninstall -y -r "${TempFreezeFile}"
+        Remove-Item -Force "${TempFreezeFile}"
+        # python -m pip freeze | %{ $_.split('==')[0] } | %{ python -m pip install --upgrade $_ }
+        if(-Not $SkipInit)
+        {
+            py_init_environmnet
+        }
+    }
+
+    ${function:srv}  = { python -m http.server 8000 }
+    if (Get-Command serv.ps1 -ErrorAction SilentlyContinue | Test-Path)
+    {
+        ${function:serv}  = { serv.ps1 }
+    }
+}
