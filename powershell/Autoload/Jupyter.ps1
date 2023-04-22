3dc7e71a8da3216736783a01693a46944bfd2095
Dmitry Ivanov <d.k.ivanov@live.com>
2023-04-22 17:57:39 +0200

    update jupyter functions

--- powershell/Autoload/Jupyter.ps1
+++ powershell/Autoload/Jupyter.ps1
@@ -17,163 +17,8 @@ if ($MyInvocation.InvocationName -ne '.')
 
 ${function:jpn} = { jupyter notebook @args }
 
-function jp
-{
-    <#
-    .SYNOPSIS
-        Wrapprer for Jupyter notebooks
-    .DESCRIPTION
-        Run and manage Jupyter environment in ~/.jpenv
-    .PARAMETER NoteBookPath
-        Path to Jupyter Notebook file.
-    .PARAMETER Command
-        Run specific commant for Jupyter. Default: 'notebook'
-    .PARAMETER Port
-        Specify non-default port to start Jupyter Notebook.
-    .PARAMETER ReInstall
-        Remove old Jupyter environment.
-    .PARAMETER KeepEnv
-        Do not deactivate virtual environment on exit.
-    .EXAMPLE
-        jp C:\Temp\Hello-World.ipynb
-        jp .\Hello-World.ipynb
-    .INPUTS
-        String
-        String
-        Int
-        Switch
-        Switch
-    .OUTPUTS
-        None
-    .NOTES
-        Written by: Dmitry Ivanov
-    #>
-    [CmdletBinding()]
-    param
-    (
-        [string]$NoteBookPath = $null,
-        [string]$Command = "notebook",
-        [Int]$Port = 8888,
-        [switch]$ReInstall,
-        [switch]$KeepEnv
-    )
-
-    if (-Not (Get-Command python -ErrorAction SilentlyContinue | Test-Path))
-    {
-        Write-Host "ERROR: Python doesn't found in system Path. Exiting..." -ForegroundColor Red
-        break
-    }
-
-    if (-Not (Get-Command virtualenv -ErrorAction SilentlyContinue | Test-Path))
-    {
-        python -m pip install virtualenv
-    }
-
-    $jenvDir = Join-Path $env:USERPROFILE .jpenv
-    if ($ReInstall -And (Test-Path $jenvDir))
-    {
-        Remove-Item -Recurse -Force $jenvDir
-    }
-
-    if (-Not (Test-Path $jenvDir))
-    {
-        $python = Get-Command python | Select-Object -ExpandProperty Definition
-        python -m virtualenv -p $python $jenvDir
-    }
-
-    & $jenvDir\Scripts\activate.ps1
-
-    if (-Not (Get-Command jupyter -ErrorAction SilentlyContinue | Test-Path))
-    {
-        python -m pip install jupyter
-    }
-
-    if ($NoteBookPath)
-    {
-        jupyter $Command --port $Port $NoteBookPath
-    }
-    else
-    {
-        jupyter $Command --port $Port
-    }
-
-    if (-Not $KeepEnv)
-    {
-        deactivate
-    }
-}
-
-function jpactivate
-{
-    <#
-    .SYNOPSIS
-        Jupyter environment configureation.
-    .DESCRIPTION
-        Jupyter environment configureation. Just activates python's virual env.
-    .PARAMETER ReInstall
-        Remove environment and install from scratch
-    .EXAMPLE
-        jpactivate
-        jpactivate -ReInstall
-    .INPUTS
-        Switch
-    .OUTPUTS
-        None
-    .NOTES
-        Written by: Dmitry Ivanov
-    #>
-    [CmdletBinding()]
-    param
-    (
-        [switch]$ReInstall
-    )
-
-    if (-Not (Get-Command python -ErrorAction SilentlyContinue | Test-Path))
-    {
-        Write-Host "ERROR: Python doesn't found in system Path. Exiting..." -ForegroundColor Red
-        break
-    }
-
-    if (-Not (Get-Command virtualenv -ErrorAction SilentlyContinue | Test-Path))
-    {
-        python -m pip install virtualenv
-    }
-
-    $jenvDir = Join-Path $env:USERPROFILE .jpenv
-    if ($ReInstall -And (Test-Path $jenvDir) )
-    {
-        Remove-Item -Recurse -Force $jenvDir
-    }
-
-    if (-Not (Test-Path $jenvDir))
-    {
-        $python = Get-Command python | Select-Object -ExpandProperty Definition
-        python -m virtualenv -p $python $jenvDir
-    }
-
-    # Set-Location $jenvDir
-    & $jenvDir\Scripts\activate.ps1
-}
-
 function jpinstall
 {
-    <#
-    .SYNOPSIS
-        Install or update my set of module for Jupiter Nortebookss.
-    .DESCRIPTION
-        Install or update my set of modules for Jupiter Nortebooks.
-    .PARAMETER ReInstall
-        Remove environment and install from scratch
-    .EXAMPLE
-        jpinstall
-        jpinstall -ReInstall
-    .INPUTS
-        Switch
-    .OUTPUTS
-        None
-    .NOTES
-        Written by: Dmitry Ivanov
-    #>
     [CmdletBinding()]
     param
     (
@@ -188,6 +33,7 @@ function jpinstall
 
     if (-Not (Get-Command virtualenv -ErrorAction SilentlyContinue | Test-Path))
     {
+        python -m pip install --upgrade pip
         python -m pip install virtualenv
     }
 
@@ -200,6 +46,7 @@ function jpinstall
     if (-Not (Test-Path $jenvDir))
     {
         $python = Get-Command python | Select-Object -ExpandProperty Definition
+        python -m pip install --upgrade pip
         python -m virtualenv -p $python $jenvDir
     }
 
@@ -220,23 +67,93 @@ function jpinstall
     python -m pip install --upgrade sklearn
     # python -m pip install --upgrade powershell_kernel
     # python -m powershell_kernel.install
+
+    # For inlined interactice plots:
+    # python -m pip install --upgrade ipympl
+    # jupyter nbextension install --py --symlink --sys-prefix --overwrite ipympl
+    # jupyter nbextension enable --py --sys-prefix ipympl
+
     deactivate
+}
+
+function jpactivate
+{
+    [CmdletBinding()]
+    param
+    (
+        [switch]$ReInstall
+    )
+
+    if (-Not (Get-Command python -ErrorAction SilentlyContinue | Test-Path))
+    {
+        Write-Host "ERROR: Python doesn't found in system Path. Exiting..." -ForegroundColor Red
+        break
+    }
+
+    $jenvDir = Join-Path $env:USERPROFILE .jpenv
+    if ($ReInstall -And (Test-Path $jenvDir))
+    {
+        Remove-Item -Recurse -Force $jenvDir
+    }
+
+    if (-Not (Test-Path $jenvDir))
+    {
+        jpinstall
+    }
+
+    # Set-Location $jenvDir
+    & $jenvDir\Scripts\activate.ps1
+}
+Set-Alias jj jpactivate
+
+function jpweb
+{
+    [CmdletBinding()]
+    param
+    (
+        [string]$NoteBookPath = $null,
+        [string]$Command = "notebook",
+        [Int]$Port = 8888,
+        [switch]$ReInstall,
+        [switch]$KeepEnv
+    )
+
+    if (-Not (Get-Command python -ErrorAction SilentlyContinue | Test-Path))
+    {
+        Write-Host "ERROR: Python doesn't found in system Path. Exiting..." -ForegroundColor Red
+        break
+    }
+
+    $jenvDir = Join-Path $env:USERPROFILE .jpenv
+    if ($ReInstall -And (Test-Path $jenvDir))
+    {
+        Remove-Item -Recurse -Force $jenvDir
+    }
+
+    if (-Not (Test-Path $jenvDir))
+    {
+        jpinstall
+    }
+
+    & $jenvDir\Scripts\activate.ps1
+
+    if ($NoteBookPath)
+    {
+        jupyter $Command --port $Port $NoteBookPath
+    }
+    else
+    {
+        jupyter $Command --port $Port
+    }
+
+    if (-Not $KeepEnv)
+    {
+        deactivate
+    }
 }
 
 function jpremove
 {
-    <#
-    .SYNOPSIS
-        Cleanup local Jupiter Environment.
-    .DESCRIPTION
-        Cleanup local Jupiter Environment.
-    .INPUTS
-        None
-    .OUTPUTS
-        None
-    .NOTES
-        Written by: Dmitry Ivanov
-    #>
     $jenvDir = Join-Path $env:USERPROFILE .jpenv
     if (Test-Path $jenvDir)
     {
@@ -256,6 +173,6 @@ function jpremove
     }
     else
     {
-        Write-Host "ERROR: Jupiter Environment: $jenvDir doesn't exist. Exiting..." -ForegroundColor Red
+        Write-Host "ERROR: Jupiter Environment: $jenvDir doesn't exist. Exiting..." -ForegroundColor Yellow
     }
 }
