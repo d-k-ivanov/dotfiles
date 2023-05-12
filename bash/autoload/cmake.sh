122369186521009672d1f267030945e1e8e8f199
Dmitry Ivanov <d.k.ivanov@live.com>
2023-05-12 11:40:26 +0200

    cgen for linux

--- bash/autoload/cmake.sh
+++ bash/autoload/cmake.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 alias csp="cp -rf ${WORKSPACE}/my/dotfiles/data/cmake/CMakePresets-Linux-GCC.json ./CMakePresets.json"
+alias cgen="cmake -G \"Ninja Multi-Config\" -B build -S . -DENABLE_TESTING=TRUE"
