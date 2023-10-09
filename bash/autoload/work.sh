23ff1cdc4e5f6345560578e1717c0ee08f6f3694
Dmitry Ivanov <d.k.ivanov@live.com>
2023-10-09 17:54:35 +0200

    add vcpkg aliases

---
+++ bash/autoload/work.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+CC_VCPKG_DEV=${HOME}/v
+CC_VCPKG_ROOT=${HOME}/vcpkg
+
+# === Current === #
+alias ccdev=" ln -sf ${HOME}/ws/vcpkg                                          ${CC_VCPKG_DEV}"
+alias ccdev5="ln -sf ${CC_VCPKG_ROOT}/a761262edcfa6bb92eab2917ac9c4382138b3bf5 ${CC_VCPKG_DEV}"
+alias ccdev6="ln -sf ${CC_VCPKG_ROOT}/fea1b6ae25a41b52e4581ff690c178d4a9224740 ${CC_VCPKG_DEV}"
+alias ccdev7="ln -sf ${CC_VCPKG_ROOT}/9c921f33c ${CC_VCPKG_DEV}"
+
+# === Legacy  === #
+alias ccdev24="ln -sf ${CC_VCPKG_ROOT}/db5bd8485aea62500c09491a959a0fe7cc254e85 ${HOME}/vcpkg-export-latest"
+alias ccdev25="ln -sf ${CC_VCPKG_ROOT}/db5bd8485aea62500c09491a959a0fe7cc254e85 ${HOME}/vcpkg-export-latest"
+alias ccdev26="ln -sf ${CC_VCPKG_ROOT}/8eaa6567bc1462280ce5200dc06391e847b5fd83 ${HOME}/vcpkg-export-latest"
+alias ccdev3=" ln -sf ${CC_VCPKG_ROOT}/a761262edcfa6bb92eab2917ac9c4382138b3bf5 ${HOME}/vcpkg-export-latest"
+alias ccdev4=" ln -sf ${CC_VCPKG_ROOT}/a761262edcfa6bb92eab2917ac9c4382138b3bf5 ${HOME}/vcpkg-export-latest"
+alias ccdev5l="ln -sf ${CC_VCPKG_ROOT}/a761262edcfa6bb92eab2917ac9c4382138b3bf5 ${HOME}/vcpkg-export-latest"
+alias ccdev6l="ln -sf ${CC_VCPKG_ROOT}/fea1b6ae25a41b52e4581ff690c178d4a9224740 ${HOME}/vcpkg-export-latest"
+
