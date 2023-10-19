33718f3d1ec90ad18191cb6a37b18de9105911b8
Dmitry Ivanov <d.k.ivanov@live.com>
2023-10-20 00:36:06 +0200

    add powershell completion for awscli

---
+++ powershell/Completion/awscli.ps1
@@ -0,0 +1,13 @@
+Register-ArgumentCompleter -Native -CommandName aws -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        $env:COMP_LINE=$wordToComplete
+        if ($env:COMP_LINE.Length -lt $cursorPosition){
+            $env:COMP_LINE=$env:COMP_LINE + " "
+        }
+        $env:COMP_POINT=$cursorPosition
+        aws_completer.exe | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+        Remove-Item Env:\COMP_LINE
+        Remove-Item Env:\COMP_POINT
+}
