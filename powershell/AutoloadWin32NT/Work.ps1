5bafbbbdb9cd5e8d847d4e7828c4e1cc508ac0f7
Dmitry Ivanov <d.k.ivanov@live.com>
2023-11-30 23:04:28 +0100

    update vcpkg scripts

--- powershell/AutoloadWin32NT/Work.ps1
+++ powershell/AutoloadWin32NT/Work.ps1
@@ -23,27 +23,27 @@ function Set-CCVCPKG
     [Environment]::SetEnvironmentVariable("CC_VCPKG_ROOT", $vcpkg_root_path , "Machine")
     $Env:CC_VCPKG_ROOT = $vcpkg_root_path
     [Environment]::SetEnvironmentVariable("CC_VCPKG_DEV", $vcpkg_dev_path, "Machine")
-    $Env:CC_VCPKG_ROOT = $vcpkg_dev_path
+    $Env:CC_VCPKG_DEV = $vcpkg_dev_path
 }
 
 # === Current === #
-${function:ccdev}   = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}dev" -Force}
+${function:ccdev}   = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\dev" -Force}
 ${function:ccdevm}  = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${Env:WORKSPACE}\vcpkg" -Force}
-${function:ccdevs}  = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\shit" -Force}
-${function:ccdev5}  = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\a761262edcfa6bb92eab2917ac9c4382138b3bf5" -Force}
-${function:ccdev60} = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\fea1b6ae25a41b52e4581ff690c178d4a9224740" -Force}
-${function:ccdev61} = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\a761262edcfa6bb92eab2917ac9c4382138b3bf5" -Force}
-${function:ccdev70} = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\9c921f33c" -Force}
-${function:ccdev71} = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\a101126ba" -Force}
+${function:ccdevs}  = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\shit" -Force}
+${function:ccdev5}  = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\a761262edcfa6bb92eab2917ac9c4382138b3bf5" -Force}
+${function:ccdev60} = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\fea1b6ae25a41b52e4581ff690c178d4a9224740" -Force}
+${function:ccdev61} = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\a761262edcfa6bb92eab2917ac9c4382138b3bf5" -Force}
+${function:ccdev70} = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\9c921f33c" -Force}
+${function:ccdev71} = { New-Item -Path "${Env:CC_VCPKG_DEV}" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\a101126ba" -Force}
 
 # === Legacy  === #
-${function:ccdev24} = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\db5bd8485aea62500c09491a959a0fe7cc254e85" -Force}
-${function:ccdev25} = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\db5bd8485aea62500c09491a959a0fe7cc254e85" -Force}
-${function:ccdev26} = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\8eaa6567bc1462280ce5200dc06391e847b5fd83" -Force}
-${function:ccdev3}  = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\a761262edcfa6bb92eab2917ac9c4382138b3bf5" -Force}
-${function:ccdev4}  = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\a761262edcfa6bb92eab2917ac9c4382138b3bf5" -Force}
-${function:ccdev5l} = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\a761262edcfa6bb92eab2917ac9c4382138b3bf5" -Force}
-${function:ccdev6l} = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${CC_VCPKG_DEV}\fea1b6ae25a41b52e4581ff690c178d4a9224740" -Force}
+${function:ccdev24} = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\db5bd8485aea62500c09491a959a0fe7cc254e85" -Force}
+${function:ccdev25} = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\db5bd8485aea62500c09491a959a0fe7cc254e85" -Force}
+${function:ccdev26} = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\8eaa6567bc1462280ce5200dc06391e847b5fd83" -Force}
+${function:ccdev3}  = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\a761262edcfa6bb92eab2917ac9c4382138b3bf5" -Force}
+${function:ccdev4}  = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\a761262edcfa6bb92eab2917ac9c4382138b3bf5" -Force}
+${function:ccdev5l} = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\a761262edcfa6bb92eab2917ac9c4382138b3bf5" -Force}
+${function:ccdev6l} = { New-Item -Path "${Env:USERPROFILE}\vcpkg-export-latest" -ItemType SymbolicLink -Value "${Env:CC_VCPKG_ROOT}\fea1b6ae25a41b52e4581ff690c178d4a9224740" -Force}
 
 # ${function:ccdev5} = { C:\Windows\System32\cmd /c mklink /d "${Env:USERPROFILE}\vcpkg-export-latest" "${Env:USERPROFILE}\vcpkg-export-5.0" }
 # ${function:ccdev6} = { C:\Windows\System32\cmd /c mklink /d "${Env:USERPROFILE}\vcpkg-export-latest" "${Env:USERPROFILE}\vcpkg-export-6.0" }
