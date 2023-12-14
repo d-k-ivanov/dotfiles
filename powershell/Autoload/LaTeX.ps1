992a4978554a8fea96e52514d18ab5f616566849
Dmitry Ivanov <d.k.ivanov@live.com>
2023-12-14 11:03:05 +0100

    add latex scripts

---
+++ powershell/Autoload/LaTeX.ps1
@@ -0,0 +1,19 @@
+<#
+.SYNOPSIS
+TeX, LaTeX, TeXLive, MkLatex, etc. scripts.
+
+.DESCRIPTION
+TeX, LaTeX, TeXLive, MkLatex, etc. scripts.
+#>
+
+# Check invocation
+if ($MyInvocation.InvocationName -ne '.')
+{
+    Write-Host `
+        "Error: Bad invocation. $($MyInvocation.MyCommand) supposed to be sourced. Exiting..." `
+        -ForegroundColor Red
+    Exit
+}
+
+${function:fix-texlive-formats-sys} = { fmtutil-sys --all  }
+${function:fix-texlive-formats-usr} = { fmtutil-user --all }
