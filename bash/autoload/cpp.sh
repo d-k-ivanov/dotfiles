e08874009faa19b786ee98666b072fe2034d0772
Dmitry Ivanov <d.k.ivanov@live.com>
2024-02-15 17:08:55 +0100

    add cppcheck aliases for linux

--- bash/autoload/cpp.sh
+++ bash/autoload/cpp.sh
@@ -10,3 +10,22 @@ clang_format_all()
 {
     find . -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format -style=file -i {} \;
 }
+
+alias cppck="cppcheck -j8 --enable=all --force"
+alias cppcki="cppck --inline-suppr"
+alias cppckif="cppcki --suppressions-list=cppcheck-suppressions.txt"
+
+cppckxml()
+{
+    cppck --xml --xml-version=2 $@ 2>cppcheck.xml
+}
+
+cppckixml()
+{
+    cppcki --xml --xml-version=2 $@ 2>cppcheck.xml
+}
+
+cppckifxml()
+{
+    cppckif --xml --xml-version=2 $@ 2>cppcheck.xml
+}
