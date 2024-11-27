044224f06075edaf96cf04e2f3e39420f854e362
Dmitry Ivanov <d.k.ivanov@live.com>
2024-11-27 01:16:13 +0100

    dotfiles: 2024-27-11-01-16

--- powershell/AutoloadWin32NT/Storage.ps1
+++ powershell/AutoloadWin32NT/Storage.ps1
@@ -6,6 +6,15 @@ Security (SSH, SSL, GPG etc.) scripts.
 Security (SSH, SSL, GPG etc.) scripts.
 #>
 
+# Check invocation
+if ($MyInvocation.InvocationName -ne '.')
+{
+    Write-Host `
+        "Error: Bad invocation. $($MyInvocation.MyCommand) supposed to be sourced. Exiting..." `
+        -ForegroundColor Red`
+    Exit
+}
+
 function Set-DropboxLocation
 {
     $StorageLocations = @(
@@ -49,13 +58,3 @@ function Set-OneDriveLocation
     [Environment]::SetEnvironmentVariable("MY_ONEDRIVE", ${Selected}, "Machine")
     Set-Item -Path Env:MY_ONEDRIVE -Value ${Selected}
 }
-
-${function:mount_meta_d} = { encfs ${env:MY_DROPBOX}\.meta M: }
-${function:umount_meta_d} = { dokanctl /u M: }
-Set-Alias -Name mmmm -Value mount_meta_d
-Set-Alias -Name mmmu -Value umount_meta_d
-
-${function:mount_meta_o} = { encfs ${env:MY_ONEDRIVE}\.meta O: }
-${function:umount_meta_o} = { dokanctl /u O: }
-Set-Alias -Name ooom -Value mount_meta_o
-Set-Alias -Name ooou -Value umount_meta_o
