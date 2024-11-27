dae1b554570f770eab9a0a20cbf8229734554170
Dmitry Ivanov <d.k.ivanov@live.com>
2024-11-27 02:37:14 +0100

    dotfiles: 2024-11-27-02-37

--- bash/autoload/bash.sh
+++ bash/autoload/bash.sh
@@ -10,7 +10,6 @@ export HISTFILESIZE=1000000
 # Ignore some commands
 export HISTIGNORE='&:ls:[bf]g:exit:pwd:clear:mount:umount:history:l:l[1als]:lla:g:g[sdp]:g+(w):gp[lp]:gppa:mm[mu]:wsdf:wsdfp:wsconf:wscc+:icod *:git stas*:tm:tma:+(;):+(.): *:[ \t]*'
 
-
 # export HISTTIMEFORMAT='%s '
 unset HISTTIMEFORMAT
 
