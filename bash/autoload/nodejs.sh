2165e1fe3d153f780a04436fd154db5c8de22128
Dmitry Ivanov <d.k.ivanov@live.com>
2024-11-27 21:17:31 +0100

    dotfiles: 2024-11-27-21-17

--- bash/autoload/nodejs.sh
+++ bash/autoload/nodejs.sh
@@ -10,3 +10,7 @@ alias npm-list-local-outdated='npm outdated --depth=0'
 alias npm-list-global-outdated='npm outdated -g --depth=0'
 alias npm-update-local='npm update'
 alias npm-update-global='npm update -g'
+
+export NVM_DIR="$HOME/.nvm"
+[[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+[[ -s "$NVM_DIR/bash_completion" ]] && source "$NVM_DIR/bash_completion"
--- bashrc
+++ bashrc
@@ -15,7 +15,6 @@ fi
 
 source ${HOME}/.bash/bash-preexec.sh
 
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+#THIS MUST BE AT THE END OF DOTFILES FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-
