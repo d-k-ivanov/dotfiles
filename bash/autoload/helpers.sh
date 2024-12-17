8554bbe1c655c75fbcf2596a2dbd59e97197a27a
Dmitry Ivanov <d.k.ivanov@live.com>
2024-12-17 13:13:53 +0100

    add bash function to install pytorch

--- bash/autoload/helpers.sh
+++ bash/autoload/helpers.sh
@@ -1,20 +1,51 @@
 #!/usr/bin/env bash
 
-print_error()
-{
-    local R="\033[0;31m"    # Red
-    local ZZ="\033[0m"      # Reset
+print_error() {
+    local R="\033[0;31m" # Red
+    local ZZ="\033[0m"   # Reset
     printf "${R}${@}${ZZ}\n"
 }
 
-print_success()
-{
-    local G="\033[0;32m"    # Green
-    local ZZ="\033[0m"      # Reset
+print_success() {
+    local G="\033[0;32m" # Green
+    local ZZ="\033[0m"   # Reset
     printf "${G}${@}${ZZ}\n"
 }
 
-show()
-{
+show() {
     type $@
+}
+
+function select_from_list() {
+    local prompt="$1" outvar="$2"
+    shift
+    shift
+    local options=("$@") cur=0 count=${#options[@]} index=0
+    local esc=$(echo -en "\e") # cache ESC as test doesn't allow esc codes
+    printf "$prompt\n"
+    while true; do
+        # list all options (option list is zero-based)
+        index=0
+        for o in "${options[@]}"; do
+            if [ "$index" == "$cur" ]; then
+                echo -e " >\e[7m$o\e[0m" # mark & highlight the current option
+            else
+                echo "  $o"
+            fi
+            ((index++))
+        done
+        read -s -n3 key               # wait for user to key in arrows or ENTER
+        if [[ $key == $esc[A ]]; then # up arrow
+            ((cur--))
+            ((cur < 0)) && ((cur = 0))
+        elif [[ $key == $esc[B ]]; then # down arrow
+            ((cur++))
+            ((cur >= count)) && ((cur = count - 1))
+        elif [[ $key == "" ]]; then # nothing, i.e the read delimiter - ENTER
+            break
+        fi
+        echo -en "\e[${count}A" # go up to the beginning to re-render
+    done
+    # export the selection to the requested output variable
+    printf -v $outvar "${options[$cur]}"
 }
--- bash/autoload/pytorch.sh
+++ bash/autoload/pytorch.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-install-libtorch() {
+install_libtorch() {
     declare -a torch_versions=(
         "2.5.1"
     )
+    select_from_list "PyTorch version:" selected_torch "${torch_versions[@]}"
 
     declare -a compute_platforms=(
         "cpu"
@@ -11,5 +12,15 @@ install-libtorch() {
         "cu121"
         "cu124"
     )
+    select_from_list "Computing platform" selected_platform "${compute_platforms[@]}"
 
+    echo "Installing PyTorch ${selected_torch} for ${selected_platform} to /opt..."
+    if [ -d "/opt/libtorch-${selected_torch}+${selected_platform}" ]; then
+        print_success "PyTorch ${selected_torch} for ${selected_platform} already installed."
+        return
+    fi
+    sudo wget https://download.pytorch.org/libtorch/${selected_platform}/libtorch-cxx11-abi-shared-with-deps-${selected_torch}%2B${selected_platform}.zip
+    sudo unzip libtorch-cxx11-abi-shared-with-deps-${selected_torch}+${selected_platform}.zip
+    sudo mv libtorch /opt/libtorch-${selected_torch}+${selected_platform}
+    sudo rm libtorch-cxx11-abi-shared-with-deps-${selected_torch}+${selected_platform}.zip
 }
