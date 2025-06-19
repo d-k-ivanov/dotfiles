e03d97c558692075973176aaf4065c5f0b29dd71
Dmitry Ivanov <d.k.ivanov@live.com>
2025-06-19 13:20:39 +0200

    add save parameter to set-libtorch function

--- bash/autoload/pytorch.sh
+++ bash/autoload/pytorch.sh
@@ -34,6 +34,45 @@ install-libtorch() {
 }
 
 set-libtorch() {
+    local save=0
+    local help=0
+    # Parse options
+    # --save|-s   Save the selected LibTorch version to the autoload file
+    # --help|-h   Show help message
+    local options=$(getopt -l "help,save" -o "hs" -a -- "$@")
+    if [ $? -ne 0 ]; then
+        echo "Invalid options provided. Use -h or --help for usage information."
+        return 1
+    fi
+    eval set -- "${options}"
+    while true; do
+        case "$1" in
+        -s | --save)
+            save=1
+            shift
+            ;;
+        -h | --help)
+            help=1
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Invalid option: $1"
+            return 1
+            ;;
+        esac
+    done
+
+    if [ ${help} -eq 1 ]; then
+        echo "Usage: set-libtorch [-s] [-h]"
+        echo "  -s, --save   Save the selected LibTorch version to the autoload file"
+        echo "  -h, --help   Show this help message"
+        return 0
+    fi
+
     declare -a libtorch_locations=($(ls -dl /opt/libtorch-* | awk '{print $9}'))
     select_from_list "Installed LibTorch:" selected "${libtorch_locations[@]}"
     echo "Using LibTorch: ${selected}..."
@@ -42,17 +81,19 @@ set-libtorch() {
     export LD_LIBRARY_PATH_ORIG="${LD_LIBRARY_PATH}"
     export LD_LIBRARY_PATH="${selected}/lib:${LD_LIBRARY_PATH}"
 
-    # Save exported variable to the autoload file: ${HOME}/.bash_local/autoload/pytorch.sh (create it if it does not exist)
-    if [ ! -d "${HOME}/.bash_local/autoload" ]; then
-        mkdir -p "${HOME}/.bash_local/autoload"
+    if [ ${save} -eq 1 ]; then
+        # Save exported variable to the autoload file: ${HOME}/.bash_local/autoload/pytorch.sh (create it if it does not exist)
+        if [ ! -d "${HOME}/.bash_local/autoload" ]; then
+            mkdir -p "${HOME}/.bash_local/autoload"
+        fi
+        echo "export LIBTORCH=\"${LIBTORCH}\"" >"${HOME}/.bash_local/autoload/pytorch.sh"
+        echo "export LIBTORCH_DIR=\"${LIBTORCH_DIR}\"" >>"${HOME}/.bash_local/autoload/pytorch.sh"
+        echo "export LD_LIBRARY_PATH_ORIG=\"${LD_LIBRARY_PATH_ORIG}\"" >>"${HOME}/.bash_local/autoload/pytorch.sh"
+        echo "export LD_LIBRARY_PATH=\"${LD_LIBRARY_PATH}\"" >>"${HOME}/.bash_local/autoload/pytorch.sh"
     fi
-    echo "export LIBTORCH=\"${LIBTORCH}\"" >"${HOME}/.bash_local/autoload/pytorch.sh"
-    echo "export LIBTORCH_DIR=\"${LIBTORCH_DIR}\"" >>"${HOME}/.bash_local/autoload/pytorch.sh"
-    echo "export LD_LIBRARY_PATH_ORIG=\"${LD_LIBRARY_PATH_ORIG}\"" >>"${HOME}/.bash_local/autoload/pytorch.sh"
-    echo "export LD_LIBRARY_PATH=\"${LD_LIBRARY_PATH}\"" >>"${HOME}/.bash_local/autoload/pytorch.sh"
 }
 
-clear-libtorch() {
+unset-libtorch() {
     unset LIBTORCH
     unset LIBTORCH_DIR
     if [ -n "${LD_LIBRARY_PATH_ORIG}" ]; then
