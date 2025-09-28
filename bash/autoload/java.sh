19a60e8f64c8b80e25cf6a1a79ec59cc00d2cbd6
Dmitry Ivanov <d.k.ivanov@live.com>
2025-09-28 22:16:43 +0200

    dotfiles: 2025-09-28-22-16

--- bash/autoload/java.sh
+++ bash/autoload/java.sh
@@ -3,3 +3,23 @@
 # SDKMAN
 # export SDKMAN_DIR="$HOME/.sdkman"
 # [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+function sdk-java-home-current()
+{
+    export JAVA_HOME=${SDKMAN_DIR}/candidates/java/current
+}
+
+function sdk-java-home-verion()
+{
+    if [ -z "$1" ]; then
+        echo "Usage: sdk-java-home-version <version>"
+        return 1
+    fi
+
+    if [ ! -d "${SDKMAN_DIR}/candidates/java/$1" ]; then
+        echo "Version $1 not found in SDKMAN"
+        return 1
+    fi
+    export JAVA_HOME=${SDKMAN_DIR}/candidates/java/$1
+}
+
