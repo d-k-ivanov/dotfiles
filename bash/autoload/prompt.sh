cba7fbc5cdac498c1980f80e2617b8d0f4ca98cb
Dmitry Ivanov <d.k.ivanov@live.com>
2025-11-07 13:24:46 +0100

    refactror starship initializer

--- bash/autoload/prompt.sh
+++ bash/autoload/prompt.sh
@@ -4,178 +4,172 @@ if [[ -n "$VSCODE_INJECTION" ]] || [[ "$TERM_PROGRAM" == "vscode" ]] || [[ -n "$
     export IS_IN_VSCODE=1
 fi
 
-if [ -z "$IS_IN_VSCODE" ] && command -v starship >/dev/null 2>&1; then
-    if [ -f "$HOME/.config/starship/starship.toml" ]; then
-        export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
+function devops {
+    if command -v starship >/dev/null 2>&1; then
+        if [ -f "$HOME/.config/starship/starship.toml" ]; then
+            export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
+        fi
+        eval "$(starship init bash)"
     fi
-    eval "$(starship init bash)"
+    export DEVOPSPROMPT=1
+}
+
+function unset-prompt-vars {
+    unset PromptUserName
+    unset PromptCompName
+}
+
+function __git_prompt {
+    # preserve exit status
+    local exit=$?
+    local PROMPT
+    PROMPT="$(cat ~/.bash/var.prompt)"
+
+    # Colors:
+    local K="\033[0;30m"   # black
+    local R="\033[0;31m"   # red
+    local G="\033[0;32m"   # green
+    local Y="\033[0;33m"   # yellow
+    local B="\033[0;34m"   # blue
+    local M="\033[0;35m"   # magenta
+    local C="\033[0;36m"   # cyan
+    local W="\033[0;37m"   # white
+    local GR="\033[0;090m" # gray
+    local BK="\033[1;30m"  # bolded black
+    local BR="\033[1;31m"  # bolded red
+    local BG="\033[1;32m"  # bolded green
+    local BY="\033[1;33m"  # bolded yellow
+    local BB="\033[1;34m"  # bolded blue
+    local BM="\033[1;35m"  # bolded magenta
+    local BC="\033[1;36m"  # bolded cyan
+    local BW="\033[1;37m"  # bolded white
+    local ZZ="\033[0m"     # Reset
+
+    if [ "${PROMPT}" == "COMPLEX" ]; then
+        # Classic Git-Prompt
+        # GIT_PS1_SHOWDIRTYSTATE=1
+        # GIT_PS1_STATESEPARATOR=""
+        # GIT_PS1_SHOWUNTRACKEDFILES=1
+        # GIT_PS1_SHOWSTASHSTATE=1
+        # GIT_PS1_SHOWCOLORHINTS=1
+        # [ $(git config user.pair) ] && GIT_PS1_PAIR="$(git config user.pair)@"
+        # __git_ps1 "${BY}[${BC}$GIT_PS1_PAIR%s${BY}]${ZZ}"
+
+        # Git Prompt from Git-Posh
+        local gitstring
+        gitstring=$(__posh_git_echo)
+        echo -en "$gitstring"
+    else
+        # Classic Git-Prompt
+        GIT_PS1_SHOWDIRTYSTATE=1
+        GIT_PS1_STATESEPARATOR=""
+        # GIT_PS1_SHOWUNTRACKEDFILES=1
+        # GIT_PS1_SHOWSTASHSTATE=1
+        # GIT_PS1_SHOWCOLORHINTS=1
+
+        [ "$(git config user.pair)" ] && GIT_PS1_PAIR="$(git config user.pair)@"
+        # __git_ps1 "${BY}[${BC}$GIT_PS1_PAIR%s${BY}]${ZZ}"
+        __git_ps1 "${BC}$GIT_PS1_PAIR%s${ZZ}" | sed 's/ \([+*%]\{1,\}\)$/\1/'
+    fi
+
+    return $exit
+}
+
+function __prompt_rvm {
+    # preserve exit status
+    local exit=$?
+    local PROMPT
+    PROMPT="$(cat ~/.bash/var.prompt)"
+    if [ "$(command -v rvm-prompt)" ]; then
+        rbv=$(rvm-prompt)
+    fi
+    [[ -z ${rbv} ]] && exit
+    rbv=${rbv#ruby-}
+    [[ $rbv == *"@"* ]] || rbv="${rbv}@default"
+    if [ "${PROMPT}" == "COMPLEX" ]; then
+        echo "[ Ruby: $rbv ]"
+    else
+        echo "$rbv"
+    fi
+    return $exit
+}
+
+function __prompt_time {
+    # preserve exit status
+    local exit=$?
+    if [ -n "${timer_output}" ]; then
+        printf '%s' "${timer_output}"
+    fi
+    return $exit
+}
+
+bash_prompt() {
+    # Colors:
+    local K="\[\033[0;30m\]"   # black
+    local R="\[\033[0;31m\]"   # red
+    local G="\[\033[0;32m\]"   # green
+    local Y="\[\033[0;33m\]"   # yellow
+    local B="\[\033[0;34m\]"   # blue
+    local M="\[\033[0;35m\]"   # magenta
+    local C="\[\033[0;36m\]"   # cyan
+    local W="\[\033[0;37m\]"   # white
+    local GR="\[\033[0;090m\]" # gray
+    local BK="\[\033[1;30m\]"  # bolded black
+    local BR="\[\033[1;31m\]"  # bolded red
+    local BG="\[\033[1;32m\]"  # bolded green
+    local BY="\[\033[1;33m\]"  # bolded yellow
+    local BB="\[\033[1;34m\]"  # bolded blue
+    local BM="\[\033[1;35m\]"  # bolded magenta
+    local BC="\[\033[1;36m\]"  # bolded cyan
+    local BW="\[\033[1;37m\]"  # bolded white
+    local ZZ="\[\033[0m\]"     # Reset
+
+    local username='\u'
+    local hostname='\H'
+
+    local ENVRM
+    ENVRM="$(cat ~/.bash/var.env)"
+
+    local PROMPT
+    PROMPT="$(cat ~/.bash/var.prompt)"
+
+    # Environment:
+    if [ -n "$SSH_CLIENT" ]; then
+        local SSHIP
+        SSHIP=$(echo "$SSH_CLIENT" | awk '{print $1}')
+        local SSHPRPT="SSH from $SSHIP"
+    else
+        local SSHPRPT=""
+    fi
+
+    if [[ -v PromptUserName ]] && [[ -v PromptCompName ]]; then
+        username="${PromptUserName}"
+        hostname="${PromptCompName}"
+    fi
+
+    case $PROMPT in
+    COMPLEX)
+        if [ "$ENVRM" == "PRODUCTION" ]; then
+            # PS1="${R}[${BY}\${?}${R}] [\$(__prompt_time)${R}] [${BR}\w${R}] \$(__git_prompt) ${M}\$(__prompt_rvm) ${BR}$SSHPRPT \012${BK}\t \u@\H λ ${ZZ}"
+            PS1="${R}[${BY}\${?}${R}] [\$(__prompt_time)${R}] ${BR}\w${R} \$(__git_prompt) ${M}\$(__prompt_rvm) ${BR}$SSHPRPT \012${GR}\t ${username}@${hostname} λ ${ZZ} "
+        else
+            # PS1="${G}[${BY}\${?}${G}] [\$(__prompt_time)${G}] [${BC}\w${G}] \$(__git_prompt) ${M}\$(__prompt_rvm) ${BB}$SSHPRPT \012{BK}\t \u@\H λ ${ZZ}"
+            PS1="${G}[${BY}\${?}${G}] [\$(__prompt_time)${G}] ${BC}\w${G} \$(__git_prompt) ${M}\$(__prompt_rvm) ${BB}$SSHPRPT \012${GR}\t ${username}@${hostname} λ ${ZZ}"
+        fi
+        ;;
+    SIMPLE)
+        if [ "$ENVRM" == "PRODUCTION" ]; then
+            PS1="${R}[${BY}\${?}${R}] ${username}@${hostname}${BY}:${R}\W \$(__git_prompt) ${M}\$(__prompt_rvm) ${BK}λ ${ZZ}"
+        else
+            PS1="${G}[${BY}\${?}${G}] ${username}@${hostname}${BY}:${G}\W \$(__git_prompt) ${M}\$(__prompt_rvm) ${BK}λ ${ZZ}"
+        fi
+        ;;
+    esac
+}
+
+if [ -z "$DEVOPSPROMPT" ] && [ -z "$IS_IN_VSCODE" ] && command -v starship >/dev/null 2>&1; then
+    devops
 else
-    function unset-prompt-vars
-    {
-        unset PromptUserName
-        unset PromptCompName
-    }
-
-    function __git_prompt
-    {
-        # preserve exit status
-        local exit=$?
-        local PROMPT
-        PROMPT="$(cat ~/.bash/var.prompt)"
-
-        # Colors:
-        local  K="\033[0;30m"   # black
-        local  R="\033[0;31m"   # red
-        local  G="\033[0;32m"   # green
-        local  Y="\033[0;33m"   # yellow
-        local  B="\033[0;34m"   # blue
-        local  M="\033[0;35m"   # magenta
-        local  C="\033[0;36m"   # cyan
-        local  W="\033[0;37m"   # white
-        local GR="\033[0;090m"  # gray
-        local BK="\033[1;30m"   # bolded black
-        local BR="\033[1;31m"   # bolded red
-        local BG="\033[1;32m"   # bolded green
-        local BY="\033[1;33m"   # bolded yellow
-        local BB="\033[1;34m"   # bolded blue
-        local BM="\033[1;35m"   # bolded magenta
-        local BC="\033[1;36m"   # bolded cyan
-        local BW="\033[1;37m"   # bolded white
-        local ZZ="\033[0m"      # Reset
-
-        if [ "${PROMPT}" == "COMPLEX" ]
-        then
-            # Classic Git-Prompt
-            # GIT_PS1_SHOWDIRTYSTATE=1
-            # GIT_PS1_STATESEPARATOR=""
-            # GIT_PS1_SHOWUNTRACKEDFILES=1
-            # GIT_PS1_SHOWSTASHSTATE=1
-            # GIT_PS1_SHOWCOLORHINTS=1
-            # [ $(git config user.pair) ] && GIT_PS1_PAIR="$(git config user.pair)@"
-            # __git_ps1 "${BY}[${BC}$GIT_PS1_PAIR%s${BY}]${ZZ}"
-
-            # Git Prompt from Git-Posh
-            local gitstring
-            gitstring=$(__posh_git_echo)
-            echo -en "$gitstring"
-        else
-            # Classic Git-Prompt
-            GIT_PS1_SHOWDIRTYSTATE=1
-            GIT_PS1_STATESEPARATOR=""
-            # GIT_PS1_SHOWUNTRACKEDFILES=1
-            # GIT_PS1_SHOWSTASHSTATE=1
-            # GIT_PS1_SHOWCOLORHINTS=1
-
-            [ "$(git config user.pair)" ] && GIT_PS1_PAIR="$(git config user.pair)@"
-            # __git_ps1 "${BY}[${BC}$GIT_PS1_PAIR%s${BY}]${ZZ}"
-            __git_ps1 "${BC}$GIT_PS1_PAIR%s${ZZ}" | sed 's/ \([+*%]\{1,\}\)$/\1/'
-        fi
-
-        return $exit
-    }
-
-    function __prompt_rvm
-    {
-        # preserve exit status
-        local exit=$?
-        local PROMPT
-        PROMPT="$(cat ~/.bash/var.prompt)"
-        if [ "$(command -v rvm-prompt)" ]
-        then
-            rbv=$(rvm-prompt)
-        fi
-        [[ -z ${rbv} ]] && exit
-        rbv=${rbv#ruby-}
-        [[ $rbv == *"@"* ]] || rbv="${rbv}@default"
-        if [ "${PROMPT}" == "COMPLEX" ]
-        then
-            echo "[ Ruby: $rbv ]"
-        else
-            echo "$rbv"
-        fi
-        return $exit
-    }
-
-    function __prompt_time
-    {
-        # preserve exit status
-        local exit=$?
-        if [ -n "${timer_output}" ]
-        then
-            printf '%s' "${timer_output}"
-        fi
-        return $exit
-    }
-
-    bash_prompt()
-    {
-        # Colors:
-        local  K="\[\033[0;30m\]"   # black
-        local  R="\[\033[0;31m\]"   # red
-        local  G="\[\033[0;32m\]"   # green
-        local  Y="\[\033[0;33m\]"   # yellow
-        local  B="\[\033[0;34m\]"   # blue
-        local  M="\[\033[0;35m\]"   # magenta
-        local  C="\[\033[0;36m\]"   # cyan
-        local  W="\[\033[0;37m\]"   # white
-        local GR="\[\033[0;090m\]"  # gray
-        local BK="\[\033[1;30m\]"   # bolded black
-        local BR="\[\033[1;31m\]"   # bolded red
-        local BG="\[\033[1;32m\]"   # bolded green
-        local BY="\[\033[1;33m\]"   # bolded yellow
-        local BB="\[\033[1;34m\]"   # bolded blue
-        local BM="\[\033[1;35m\]"   # bolded magenta
-        local BC="\[\033[1;36m\]"   # bolded cyan
-        local BW="\[\033[1;37m\]"   # bolded white
-        local ZZ="\[\033[0m\]"      # Reset
-
-        local username='\u'
-        local hostname='\H'
-
-        local ENVRM
-        ENVRM="$(cat ~/.bash/var.env)"
-
-        local PROMPT
-        PROMPT="$(cat ~/.bash/var.prompt)"
-
-        # Environment:
-        if [ -n "$SSH_CLIENT" ]
-        then
-            local SSHIP
-            SSHIP=$(echo "$SSH_CLIENT" | awk '{print $1}')
-            local SSHPRPT="SSH from $SSHIP"
-        else
-            local SSHPRPT=""
-        fi
-
-        if [[ -v PromptUserName ]] && [[ -v PromptCompName ]]
-        then
-            username="${PromptUserName}"
-            hostname="${PromptCompName}"
-        fi
-
-        case $PROMPT in
-            COMPLEX)
-                if [ "$ENVRM" == "PRODUCTION" ]
-                then
-                    # PS1="${R}[${BY}\${?}${R}] [\$(__prompt_time)${R}] [${BR}\w${R}] \$(__git_prompt) ${M}\$(__prompt_rvm) ${BR}$SSHPRPT \012${BK}\t \u@\H λ ${ZZ}"
-                    PS1="${R}[${BY}\${?}${R}] [\$(__prompt_time)${R}] ${BR}\w${R} \$(__git_prompt) ${M}\$(__prompt_rvm) ${BR}$SSHPRPT \012${GR}\t ${username}@${hostname} λ ${ZZ} "
-                else
-                    # PS1="${G}[${BY}\${?}${G}] [\$(__prompt_time)${G}] [${BC}\w${G}] \$(__git_prompt) ${M}\$(__prompt_rvm) ${BB}$SSHPRPT \012{BK}\t \u@\H λ ${ZZ}"
-                    PS1="${G}[${BY}\${?}${G}] [\$(__prompt_time)${G}] ${BC}\w${G} \$(__git_prompt) ${M}\$(__prompt_rvm) ${BB}$SSHPRPT \012${GR}\t ${username}@${hostname} λ ${ZZ}"
-                fi
-                ;;
-            SIMPLE)
-                if [ "$ENVRM" == "PRODUCTION" ]
-                then
-                    PS1="${R}[${BY}\${?}${R}] ${username}@${hostname}${BY}:${R}\W \$(__git_prompt) ${M}\$(__prompt_rvm) ${BK}λ ${ZZ}"
-                else
-                    PS1="${G}[${BY}\${?}${G}] ${username}@${hostname}${BY}:${G}\W \$(__git_prompt) ${M}\$(__prompt_rvm) ${BK}λ ${ZZ}"
-                fi
-                ;;
-        esac
-    }
-
     preexec_functions+=(timer_start)
     precmd_functions+=(timer_stop)
     bash_prompt
--- powershell/AutoloadWin32NT/Prompt.ps1
+++ powershell/AutoloadWin32NT/Prompt.ps1
@@ -20,137 +20,161 @@ if (Get-Module PSReadline -ErrorAction "SilentlyContinue")
     Set-PSReadLineOption -ExtraPromptLineCount 1
 }
 
-if ($Env:VSCODE_INJECTION -or $Env:TERM_PROGRAM -eq "vscode" -or $Env:VSCODE_PID) {
+if ($Env:VSCODE_INJECTION -or $Env:TERM_PROGRAM -eq "vscode" -or $Env:VSCODE_PID)
+{
     $Env:IS_IN_VSCODE = "1"
 }
 
-if (-Not $Env:IS_IN_VSCODE -And (Get-Command starship -ErrorAction SilentlyContinue | Test-Path))
+function CheckGit($Path)
 {
-    if (Test-Path "${Env:USERPROFILE}\.config\starship\starship.toml")
+    if (Test-Path -Path (Join-Path $Path '.git'))
     {
-        $Env:STARSHIP_CONFIG = "${Env:USERPROFILE}\.config\starship\starship.toml"
+        Write-VcsStatus
+        return
     }
-    Invoke-Expression (& 'starship' init powershell --print-full-init | Out-String)
+    $SplitPath = Split-Path $Path
+    if ($SplitPath)
+    {
+        CheckGit($SplitPath)
+    }
+}
+
+function unset-prompt-vars()
+{
+    Remove-Item Env:PromptUserName
+    Remove-Item Env:PromptCompName
+}
+
+
+# ==========================================================================================
+# Prompt Hooks
+# ==========================================================================================
+[ScriptBlock]$ExecutionTime = {
+    $history = Get-History -ErrorAction Ignore -Count 1
+    if ($history)
+    {
+        Microsoft.PowerShell.Utility\Write-Host "[" -NoNewline -ForegroundColor DarkGreen
+        $ts = New-TimeSpan $history.StartExecutionTime $history.EndExecutionTime
+        switch ($ts)
+        {
+            { $_.TotalMilliseconds -lt 1 }
+            {
+                [int]$us = $(($_.Ticks / 10) % 1000)
+                '{0:d3}µs' -f ($us) | Microsoft.PowerShell.Utility\Write-Host -ForegroundColor DarkGray -NoNewline
+                break
+            }
+            { $_.TotalMilliseconds -lt 100 }
+            {
+                [int]$ms = $_.TotalMilliseconds
+                [int]$us = $(($_.Ticks / 10) % 1000)
+                '{0}.{1:d3}ms' -f ($ms, $us) | Microsoft.PowerShell.Utility\Write-Host -ForegroundColor Cyan -NoNewline
+                break
+            }
+            { $_.TotalSeconds -lt 1 }
+            {
+                [int]$ms = $_.TotalMilliseconds
+                '{0}ms' -f ($ms) | Microsoft.PowerShell.Utility\Write-Host -ForegroundColor Cyan -NoNewline
+                break
+            }
+            { $_.TotalSeconds -lt 5 }
+            {
+                [int]$s = [Math]::Floor([decimal]($_.TotalSeconds))
+                [int]$ms = $($_.TotalMilliseconds % 1000)
+                '{0}.{1:d3}s' -f ($s, $ms) | Microsoft.PowerShell.Utility\Write-Host -ForegroundColor Cyan -NoNewline
+                break
+            }
+            { $_.TotalMinutes -lt 1 }
+            {
+                [int]$s = [Math]::Floor([decimal]($_.TotalSeconds))
+                [int]$ms = $($_.TotalMilliseconds % 1000)
+                '{0}.{1:d3}s' -f ($s, $ms) | Microsoft.PowerShell.Utility\Write-Host -ForegroundColor Yellow -NoNewline
+                break
+            }
+            { $_.TotalMinutes -ge 1 }
+            {
+                "{0:HH:mm:ss}" -f ([datetime]$ts.Ticks) | Microsoft.PowerShell.Utility\Write-Host -ForegroundColor Red -NoNewline
+                break
+            }
+        }
+        Microsoft.PowerShell.Utility\Write-Host "] " -NoNewline -ForegroundColor DarkGreen
+    }
+}
+
+[ScriptBlock]$GitPrompt = {}
+
+# ==========================================================================================
+# This scriptblock runs every time the prompt is returned.
+# Explicitly use functions from MS namespace to protect from being overridden in the user session.
+# Custom prompt functions are loaded in as constants to get the same behaviour
+# ==========================================================================================
+[ScriptBlock]$Prompt = {
+    $realLASTEXITCODE = $LASTEXITCODE
+    Microsoft.PowerShell.Utility\Write-Host "[" -NoNewline -ForegroundColor DarkGreen
+    Microsoft.PowerShell.Utility\Write-Host "$realLASTEXITCODE" -NoNewline -ForegroundColor Yellow
+    Microsoft.PowerShell.Utility\Write-Host "] " -NoNewline -ForegroundColor DarkGreen
+
+    ExecutionTime | Microsoft.PowerShell.Utility\Write-Host -NoNewline
+
+    $host.UI.RawUI.WindowTitle = Microsoft.PowerShell.Management\Split-Path $pwd.ProviderPath -Leaf
+    $Host.UI.RawUI.ForegroundColor = "White"
+
+    # Microsoft.PowerShell.Utility\Write-Host "[" -NoNewline -ForegroundColor DarkGreen
+    Microsoft.PowerShell.Utility\Write-Host $pwd.ProviderPath -NoNewline -ForegroundColor Cyan
+    # Microsoft.PowerShell.Utility\Write-Host "]" -NoNewline -ForegroundColor DarkGreen
+
+    Microsoft.PowerShell.Utility\Write-Host $(checkGit($pwd.ProviderPath)) -NoNewline
+    Microsoft.PowerShell.Utility\Write-Host "" ${Env:CONDA_PROMPT_MODIFIER} -NoNewline -ForegroundColor DarkYellow
+
+    $now = Get-Date -Format "HH:mm:ss"
+    if ($Env:PromptUserName -and $Env:PromptCompName)
+    {
+        $username = $Env:PromptUserName
+        $compname = $Env:PromptCompName
+    }
+    else
+    {
+        # $username = $(Split-Path (Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object UserName).UserName -Leaf)
+        $username = Split-Path $Env:USERPROFILE -Leaf
+        $compname = (Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object DNSHostName).DNSHostName
+    }
+    Microsoft.PowerShell.Utility\Write-Host "`n${now} ${username}@${compname} λ " -NoNewline -ForegroundColor DarkGray
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return " "
+}
+
+function Set-Promt
+{
+    Set-Item -Path function:\GitPrompt      -Value $GitPrompt
+    Set-Item -Path function:\ExecutionTime  -Value $ExecutionTime
+    Set-Item -Path function:\Prompt         -Value $Prompt
+}
+
+function devops
+{
+    if (Get-Command starship -ErrorAction SilentlyContinue | Test-Path)
+    {
+        if (Test-Path "${Env:USERPROFILE}\.config\starship\starship.toml")
+        {
+            $Env:STARSHIP_CONFIG = "${Env:USERPROFILE}\.config\starship\starship.toml"
+        }
+        Invoke-Expression (& 'starship' init powershell --print-full-init | Out-String)
+    }
+    Set-Item -Path Env:DEVOPSPROMPT -Value "1"
+}
+
+function undevops
+{
+    Remove-Item Env:DEVOPSPROMPT -ErrorAction SilentlyContinue
+    Set-Promt
+}
+
+if ($Env:DEVOPSPROMPT -and -not $Env:IS_IN_VSCODE -and (Get-Command starship -ErrorAction SilentlyContinue | Test-Path))
+{
+    devops
 }
 else
 {
-    function CheckGit($Path)
-    {
-        if (Test-Path -Path (Join-Path $Path '.git'))
-        {
-            Write-VcsStatus
-            return
-        }
-        $SplitPath = Split-Path $Path
-        if ($SplitPath)
-        {
-            CheckGit($SplitPath)
-        }
-    }
-
-    function unset-prompt-vars()
-    {
-        Remove-Item Env:PromptUserName
-        Remove-Item Env:PromptCompName
-    }
-
-    # ==========================================================================================
-    # Prompt Hooks
-    # ==========================================================================================
-    [ScriptBlock]$ExecutionTime = {
-        $history = Get-History -ErrorAction Ignore -Count 1
-        if ($history)
-        {
-            Microsoft.PowerShell.Utility\Write-Host "[" -NoNewline -ForegroundColor DarkGreen
-            $ts = New-TimeSpan $history.StartExecutionTime $history.EndExecutionTime
-            switch ($ts)
-            {
-                { $_.TotalMilliseconds -lt 1 }
-                {
-                    [int]$us = $(($_.Ticks / 10) % 1000)
-                    '{0:d3}µs' -f ($us) | Microsoft.PowerShell.Utility\Write-Host -ForegroundColor DarkGray -NoNewline
-                    break
-                }
-                { $_.TotalMilliseconds -lt 100 }
-                {
-                    [int]$ms = $_.TotalMilliseconds
-                    [int]$us = $(($_.Ticks / 10) % 1000)
-                    '{0}.{1:d3}ms' -f ($ms, $us) | Microsoft.PowerShell.Utility\Write-Host -ForegroundColor Cyan -NoNewline
-                    break
-                }
-                { $_.TotalSeconds -lt 1 }
-                {
-                    [int]$ms = $_.TotalMilliseconds
-                    '{0}ms' -f ($ms) | Microsoft.PowerShell.Utility\Write-Host -ForegroundColor Cyan -NoNewline
-                    break
-                }
-                { $_.TotalSeconds -lt 5 }
-                {
-                    [int]$s = [Math]::Floor([decimal]($_.TotalSeconds))
-                    [int]$ms = $($_.TotalMilliseconds % 1000)
-                    '{0}.{1:d3}s' -f ($s, $ms) | Microsoft.PowerShell.Utility\Write-Host -ForegroundColor Cyan -NoNewline
-                    break
-                }
-                { $_.TotalMinutes -lt 1 }
-                {
-                    [int]$s = [Math]::Floor([decimal]($_.TotalSeconds))
-                    [int]$ms = $($_.TotalMilliseconds % 1000)
-                    '{0}.{1:d3}s' -f ($s, $ms) | Microsoft.PowerShell.Utility\Write-Host -ForegroundColor Yellow -NoNewline
-                    break
-                }
-                { $_.TotalMinutes -ge 1 }
-                {
-                    "{0:HH:mm:ss}" -f ([datetime]$ts.Ticks) | Microsoft.PowerShell.Utility\Write-Host -ForegroundColor Red -NoNewline
-                    break
-                }
-            }
-            Microsoft.PowerShell.Utility\Write-Host "] " -NoNewline -ForegroundColor DarkGreen
-        }
-    }
-
-    [ScriptBlock]$GitPrompt = {}
-
-    # ==========================================================================================
-    # This scriptblock runs every time the prompt is returned.
-    # Explicitly use functions from MS namespace to protect from being overridden in the user session.
-    # Custom prompt functions are loaded in as constants to get the same behaviour
-    # ==========================================================================================
-    [ScriptBlock]$Prompt = {
-        $realLASTEXITCODE = $LASTEXITCODE
-        Microsoft.PowerShell.Utility\Write-Host "[" -NoNewline -ForegroundColor DarkGreen
-        Microsoft.PowerShell.Utility\Write-Host "$realLASTEXITCODE" -NoNewline -ForegroundColor Yellow
-        Microsoft.PowerShell.Utility\Write-Host "] " -NoNewline -ForegroundColor DarkGreen
-
-        ExecutionTime | Microsoft.PowerShell.Utility\Write-Host -NoNewline
-
-        $host.UI.RawUI.WindowTitle = Microsoft.PowerShell.Management\Split-Path $pwd.ProviderPath -Leaf
-        $Host.UI.RawUI.ForegroundColor = "White"
-
-        # Microsoft.PowerShell.Utility\Write-Host "[" -NoNewline -ForegroundColor DarkGreen
-        Microsoft.PowerShell.Utility\Write-Host $pwd.ProviderPath -NoNewline -ForegroundColor Cyan
-        # Microsoft.PowerShell.Utility\Write-Host "]" -NoNewline -ForegroundColor DarkGreen
-
-        Microsoft.PowerShell.Utility\Write-Host $(checkGit($pwd.ProviderPath)) -NoNewline
-        Microsoft.PowerShell.Utility\Write-Host "" ${Env:CONDA_PROMPT_MODIFIER} -NoNewline -ForegroundColor DarkYellow
-
-        $now = Get-Date -Format "HH:mm:ss"
-        if ($Env:PromptUserName -and $Env:PromptCompName)
-        {
-            $username = $Env:PromptUserName
-            $compname = $Env:PromptCompName
-        }
-        else
-        {
-            # $username = $(Split-Path (Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object UserName).UserName -Leaf)
-            $username = Split-Path $Env:USERPROFILE -Leaf
-            $compname = (Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object DNSHostName).DNSHostName
-        }
-        Microsoft.PowerShell.Utility\Write-Host "`n${now} ${username}@${compname} λ " -NoNewline -ForegroundColor DarkGray
-
-        $global:LASTEXITCODE = $realLASTEXITCODE
-        return " "
-    }
-
     Set-Item -Path function:\GitPrompt      -Value $GitPrompt
     Set-Item -Path function:\ExecutionTime  -Value $ExecutionTime
     Set-Item -Path function:\Prompt         -Value $Prompt
