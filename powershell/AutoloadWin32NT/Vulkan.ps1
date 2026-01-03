be09ae353b7a1d3a95ce31b529d6ea5dda03c461
Dmitry Ivanov <d.k.ivanov@live.com>
2026-01-03 21:48:36 +0100

    dotfiles: 2026-03-01-21-48

--- powershell/AutoloadWin32NT/Vulkan.ps1
+++ powershell/AutoloadWin32NT/Vulkan.ps1
@@ -12,7 +12,7 @@ if ($MyInvocation.InvocationName -ne '.')
     Write-Host `
         "Error: Bad invocation. $($MyInvocation.MyCommand) supposed to be sourced. Exiting..." `
         -ForegroundColor Red
-    Exit
+    exit
 }
 
 function Get-VulkanSDKList
@@ -36,7 +36,7 @@ function Get-VulkanSDKList
     {
         if (Test-Path $SDK)
         {
-            $SDKValidated  += $SDK
+            $SDKValidated += $SDK
         }
     }
 
@@ -52,7 +52,7 @@ function List-VulkanSDK
         Write-Host "List of Vulkan SDKs on this PC:"
         foreach ($SDK in $SDKs)
         {
-            if ($($SDK -Match '\d+.\d+.\d+.\d+'))
+            if ($($SDK -match '\d+.\d+.\d+.\d+'))
             {
                 Write-Host " - $($Matches[0]) --> $SDK"
             }
@@ -69,14 +69,22 @@ function Set-VulkanSDK
     $Selected = Select-From-List $(Get-VulkanSDKList) 'Vulkan SDK'
     [Environment]::SetEnvironmentVariable("VK_SDK_PATH", ${Selected}, "Machine")
     [Environment]::SetEnvironmentVariable("VULKAN_SDK", ${Selected}, "Machine")
+
     Set-Item -Path Env:VK_SDK_PATH -Value ${Selected}
     Set-Item -Path Env:VULKAN_SDK  -Value ${Selected}
     Set-Item -Path Env:PATH -Value "${Env:VULKAN_SDK}\Bin;${Env:PATH}"
+
+    # Set VULKAN_HOME and VULKAN_DOWNLOAD
+    $VulkanHome = Split-Path -Path ${Selected} -Parent
+    [Environment]::SetEnvironmentVariable("VUKAN_HOME", ${VulkanHome}, "Machine")
+    [Environment]::SetEnvironmentVariable("VUKAN_DOWNLOAD", "${VulkanHome}\Releases", "Machine")
+    Set-Item -Path Env:VUKAN_HOME     -Value ${VulkanHome}
+    Set-Item -Path Env:VUKAN_DOWNLOAD -Value "${VulkanHome}\Releases"
 }
 
 function Get-VulkanSDK
 {
-    if ($($Env:VULKAN_SDK -Match '\d+.\d+.\d+.\d+'))
+    if ($($Env:VULKAN_SDK -match '\d+.\d+.\d+.\d+'))
     {
         Write-Host " - $($Matches[0]) --> $Env:VULKAN_SDK"
     }
@@ -85,7 +93,13 @@ function Get-VulkanSDK
 function Clear-VulkanSDK
 {
     [Environment]::SetEnvironmentVariable("VK_SDK_PATH", [NullString]::Value, "Machine")
-    [Environment]::SetEnvironmentVariable("VULKAN_SDK",  [NullString]::Value, "Machine")
+    [Environment]::SetEnvironmentVariable("VULKAN_SDK", [NullString]::Value, "Machine")
     if ($Env:VK_SDK_PATH) { Remove-Item Env:VK_SDK_PATH }
-    if ($Env:VULKAN_SDK)  { Remove-Item Env:VULKAN_SDK  }
+    if ($Env:VULKAN_SDK) { Remove-Item Env:VULKAN_SDK }
+
+    # Clear VULKAN_HOME and VULKAN_DOWNLOAD
+    [Environment]::SetEnvironmentVariable("VUKAN_HOME", [NullString]::Value, "Machine")
+    [Environment]::SetEnvironmentVariable("VUKAN_DOWNLOAD", [NullString]::Value, "Machine")
+    if ($Env:VUKAN_HOME) { Remove-Item Env:VUKAN_HOME }
+    if ($Env:VUKAN_DOWNLOAD) { Remove-Item Env:VUKAN_DOWNLOAD }
 }
