44461c366262b6a358dfdf4da7271bdd773bb0a1
Dmitry Ivanov <d.k.ivanov@live.com>
2026-05-21 17:40:36 +0200

    dotfiles: 2026-21-05-17-40

--- bash/autoload/ansible.sh
+++ bash/autoload/ansible.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
 alias ap="ansible-playbook "
+
+function ansible_show_secret_var() {
+  local variable_name="$1"
+  local file_path="$2"
+  ansible localhost -m ansible.builtin.debug -a "var=${variable_name}" -e "@${file_path}" --ask-vault-pass
+}
+
+function ansible_show_secret_var_with_file() {
+  local variable_name="$1"
+  local file_path="$2"
+  local vault_pass_file_path="$3"
+  ansible localhost -m ansible.builtin.debug -a "var=${variable_name}" -e "@${file_path}" --vault-password-file="${vault_pass_file_path}"
+}
