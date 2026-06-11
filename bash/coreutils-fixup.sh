331463be91a9644ea0b43be17af771f9c4bfd48e
Dmitry Ivanov <d.k.ivanov@live.com>
2026-06-11 17:27:48 +0200

    add LC_ALL and LC_COLLATE to ls, dir, and vdir aliases

--- bash/coreutils-fixup.sh
+++ bash/coreutils-fixup.sh
@@ -7,46 +7,46 @@ case ${platform} in
         if [[ "$(lsb_release -rs 2>/dev/null)" == "26.04" ]]; then
             if command -v gnuls >/dev/null 2>&1; then
                 unalias ls 2>/dev/null
-                alias ls='gnuls '
+                alias ls='LC_ALL=C LC_COLLATE=C gnuls '
                 if [ -x /usr/bin/dircolors ]; then
-                    alias ls='gnuls --color=auto '
+                    alias ls='LC_ALL=C LC_COLLATE=C gnuls --color=auto '
                 fi
             fi
 
             if command -v gnudir >/dev/null 2>&1; then
                 unalias dir 2>/dev/null
-                alias dir='gnudir '
+                alias dir='LC_ALL=C LC_COLLATE=C gnudir '
                 if [ -x /usr/bin/dircolors ]; then
-                    alias dir='gnudir --color=auto '
+                    alias dir='LC_ALL=C LC_COLLATE=C gnudir --color=auto '
                 fi
             fi
 
             if command -v gnuvdir >/dev/null 2>&1; then
                 unalias vdir 2>/dev/null
-                alias vdir='gnuvdir '
+                alias vdir='LC_ALL=C LC_COLLATE=C gnuvdir '
                 if [ -x /usr/bin/dircolors ]; then
-                    alias vdir='gnuvdir --color=auto '
+                    alias vdir='LC_ALL=C LC_COLLATE=C gnuvdir --color=auto '
                 fi
             fi
         else
             if [ -x /usr/bin/dircolors ]; then
-                alias ls='ls --color=auto '
-                alias dir='dir --color=auto '
-                alias vdir='vdir --color=auto '
+                alias ls='LC_ALL=C LC_COLLATE=C ls --color=auto '
+                alias dir='LC_ALL=C LC_COLLATE=C dir --color=auto '
+                alias vdir='LC_ALL=C LC_COLLATE=C vdir --color=auto '
             fi
         fi
         ;;
     Darwin )
-        alias ls='gls --color=auto '
+        alias ls='LC_ALL=C LC_COLLATE=C gls --color=auto '
         ;;
     FreeBSD )
-        alias ls='ls -G '
+        alias ls='LC_ALL=C LC_COLLATE=C ls -G '
         ;;
     MSYS_NT-10.0 )
         if [ -x /usr/bin/dircolors ]; then
-            alias ls='ls --color=auto '
-            alias dir='dir --color=auto '
-            alias vdir='vdir --color=auto '
+            alias ls='LC_ALL=C LC_COLLATE=C ls --color=auto '
+            alias dir='LC_ALL=C LC_COLLATE=C dir --color=auto '
+            alias vdir='LC_ALL=C LC_COLLATE=C vdir --color=auto '
         fi
         ;;
 esac
