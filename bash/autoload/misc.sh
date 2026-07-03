ab602ff85350f51fc327362867b3499f300b4d93
Dmitry Ivanov <d.k.ivanov@live.com>
2026-07-03 11:32:30 +0200

    add remove duplicates function

--- bash/autoload/misc.sh
+++ bash/autoload/misc.sh
@@ -13,5 +13,7 @@ exit_code()
 # Add an "alert" alias for long running commands.  Use like so: `sleep 10; alert`
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
-alias remove_duplicates_inplace='awk -i inplace '\''!x[$0]++'\''
+function remove_duplicates_inplace() {
+    awk -i inplace '!seen[$0]++' "$@"
+}
 
