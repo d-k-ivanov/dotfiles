d71c765996020f97acf3e04367d7442c33fbfc3e
Dmitry Ivanov <d.k.ivanov@live.com>
2026-07-29 12:55:47 +0200

    add draco converters

--- powershell/AutoloadWin32NT/3D.ps1
+++ powershell/AutoloadWin32NT/3D.ps1
@@ -14,44 +14,58 @@ if ($MyInvocation.InvocationName -ne '.')
         -ForegroundColor Red
     exit
 }
-
-# Get-ChildItem . -File -Filter "*.drc" | %{ draco_decoder.exe -i "$($_.Name)" -o "$($_.BaseName).stl" }
-function draco_to_stl
+function draco_decode
 {
     [CmdletBinding()]
     param
     (
         [ValidateScript({ Test-Path -Path $_ -PathType Container })]
-        [string] $Path = '.'
+        [string] $Path = '.',
+        [string] $Format = 'stl'
     )
 
     $FullPath = Convert-Path $Path
     Set-Location $FullPath
 
     Get-ChildItem -Path $FullPath -File -Recurse -Filter *.drc | ForEach-Object {
-        $out = Join-Path $_.DirectoryName "$($_.BaseName).stl"
+        $out = Join-Path $_.DirectoryName "$($_.BaseName).$Format"
         Write-Host "Converting: $($_.FullName) -> $out" -ForegroundColor Yellow
         & draco_decoder.exe -i $_.FullName -o $out
     }
 }
-Set-Alias -Name drc_to_stl -Value draco_to_stl
 
-function stl_to_draco
+function draco_encode
 {
     [CmdletBinding()]
     param
     (
         [ValidateScript({ Test-Path -Path $_ -PathType Container })]
-        [string] $Path = '.'
+        [string] $Path = '.',
+        [string] $Format = 'stl'
     )
 
     $FullPath = Convert-Path $Path
     Set-Location $FullPath
 
-    Get-ChildItem -Path $FullPath -File -Recurse -Filter *.stl | ForEach-Object {
+    Get-ChildItem -Path $FullPath -File -Recurse -Filter *.$Format | ForEach-Object {
         $out = Join-Path $_.DirectoryName "$($_.BaseName).drc"
         Write-Host "Converting: $($_.FullName) -> $out" -ForegroundColor Yellow
         & draco_encoder.exe -i $_.FullName -o $out
     }
 }
-Set-Alias -Name stl_to_drc -Value stl_to_draco
+
+${function:drc_to_stl} = { draco_decode }
+${function:drc_to_ply} = { draco_decode -Format 'ply' }
+${function:drc_to_obj} = { draco_decode -Format 'obj' }
+
+${function:draco_to_stl} = { draco_decode }
+${function:draco_to_ply} = { draco_decode -Format 'ply' }
+${function:draco_to_obj} = { draco_decode -Format 'obj' }
+
+${function:stl_to_drc} = { draco_encode }
+${function:ply_to_drc} = { draco_encode -Format 'ply' }
+${function:obj_to_drc} = { draco_encode -Format 'obj' }
+
+${function:stl_to_draco} = { draco_encode }
+${function:ply_to_draco} = { draco_encode -Format 'ply' }
+${function:obj_to_draco} = { draco_encode -Format 'obj' }
